cbdba71ea8b4e15e80e37b8156cc70c63eeb29f2
pavani2105 <125651883+pavani2105@users.noreply.github.com>
2023-02-17 22:42:45 +0530

    Add files via upload

---
+++ openv.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+main()
+{
+	int n=20,k=5,i,a[10],s=0,v=0,x,b;
+	for(i=1;i<=n;i++)
+	{
+		if(i%k==0)
+		{
+	 
+		a[x]=i;	
+	    printf(" %d",a[x]);	
+	}
+ 
+
+} 
+  for(i=1;i<=n;i++)
+  {
+  	if(i%k==0)
+  	{
+  		a[x]=i;
+  		
+	
+	    if(a[x]%2==0)
+	     {
+	     	s=s+a[x];}
+	     	
+		 else
+		 {
+		 	v=v+a[x];
+		
+		 }
+}} 
+printf(" \n%d",v);
+printf(" %d",s);}
